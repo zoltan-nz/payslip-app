85afee5325304c1b560918812aa1e9533b8d2c2d
Zoltan <zdebre@gmail.com>
2013-11-18 21:40:42 +0000

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Payslip::Application.config.secret_key_base = '7dee75de6486f752a8afc3f71bb59a064e845e7a344993ce7e94b806a579aaf2b205033ddcf69e4b467a995c743efd37748480e4e4f871a7d882b254310b1f0b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Payslip::Application.config.session_store :cookie_store, key: '_payslip_session'
