f636d084dc2549bd7b69b3df2cc43da73e0f08d3
Zoltan <zdebre@gmail.com>
2013-11-20 23:53:45 +0000

    home page with logo

--- spec/features/root_spec.rb
+++ spec/features/root_spec.rb
@@ -2,9 +2,11 @@ require 'spec_helper'
 
 feature 'Home page' do
   scenario 'loading home page with logo brand', js: true do
-    visit root_path
-    page.should have_content('PaySlip App')
+    visit '/'
+    expect(page).to have_content 'PaySlip App'
   end
+
+  scenario ''
 end
 
 
